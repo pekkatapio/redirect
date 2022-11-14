cc54c99fd2c3cd6098f67ae2631c11ed386d3b80
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2022-11-14 13:22:41 +0200

    Tunnisteen syöttäminen kyselyjonon kautta

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 
-  header("Location: http://www.sasky.fi/");
+  // Luodaan osoitteet-taulukko
+  $osoitteet = array(
+    "NG5TG" => "https://www.w3schools.com/php/",
+    "R7E7L" => "https://www.php.net/manual/en/index.php",
+    "S44E8" => "https://thevalleyofcode.com/php/",
+    "UDCJ9" => "https://phpapprentice.com/",
+    "ZZU1M" => "https://phptherightway.com/"
+  );
+
+  // Poimitaan URL-osoitteesta hash-parametrin arvo.
+  $hash = $_GET["hash"];
+
+  // Haetaan $hash-muuttujan arvoa vastaava osoite.
+  $url = $osoitteet[$hash];
+
+  // Edelleenohjataan hash-tunnusta vastaavaan osoitteeseen.
+  header("Location: " . $url);
   exit;
 
 ?>
