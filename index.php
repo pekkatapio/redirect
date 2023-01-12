a69b3ca7e51578af9244cb496f40aa3934f67d3f
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2023-01-12 14:44:27 +0200

    Ulkoasutyylien lisääminen

--- index.php
+++ index.php
@@ -71,6 +71,7 @@
     <meta charset='UTF-8'>
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
+    <link href='styles.css' rel='stylesheet'>
   </head>
   <body>
     <div class='page'>
